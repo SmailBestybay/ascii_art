c40eea5e6fdbffc13579eeb7681a1016e042dc66
Smile <smailbestybay@gmail.com>
2023-02-14 17:17:36 -0800

    fix unnecessary empy line, add resize factor

--- ascii_art/ascii_art_app.py
+++ ascii_art/ascii_art_app.py
@@ -5,6 +5,9 @@ from functools import reduce
 im = Image.open("ascii_art/houses.jpeg")
 print("Succesfully loaded image!")
 print(f'Image size: {im.width} x {im.height}')
+resize_factor = 1
+im = im.resize((im.width // resize_factor, im.height //  resize_factor))
+print(f'Image new size: {im.width} x {im.height}')
 
 symbols = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$" # 65 chars
 im_array = []
@@ -14,9 +17,10 @@ for y in range(im.height):
     for x in range(im.width):
         brightness_level = int(reduce(operator.add, im.getpixel((x,y)))/3)
         symbol_index = int(len(symbols) * (brightness_level / 255))
-        im_array[y].append(symbols[symbol_index])
+        im_array[y].append(symbols[symbol_index-1])
 
 for row in range(len(im_array)):
     for col in range(len(im_array[row])):
         print(im_array[row][col], end='')
-    print('\n')
+        print(im_array[row][col], end='')
+    print()
