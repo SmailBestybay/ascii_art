fddcff74eb51ddaeeec9611a0387c1d3f60ffc71
Smile <smailbestybay@gmail.com>
2023-02-14 18:22:45 -0800

    add command line arguments

--- ascii_art/ascii_art_app.py
+++ ascii_art/ascii_art_app.py
@@ -1,6 +1,28 @@
 from PIL import Image
 import operator
 from functools import reduce
+import sys
+
+def average(pixel):
+    return int(reduce(operator.add, pixel) / 3)
+
+def min_max(pixel):
+    return int((max(pixel) + min(pixel)) / 2)
+
+def luminosity(pixel):
+    return int( (0.21 * pixel[0]) + (0.72 * pixel[1]) + (0.07 * pixel[2]) )
+
+modes = {'average' : average, 'min_max' : min_max, 'luminosity' : luminosity}
+
+if len(sys.argv) == 0 or len(sys.argv) > 1:
+    active_mode = modes['luminosity']
+else:
+    mode = sys.argv[1]
+    if mode in modes:
+        active_mode = modes[mode]
+    else:
+        active_mode = modes['luminosity']
+
 
 im = Image.open("ascii_art/houses.jpeg")
 print("Succesfully loaded image!")
@@ -15,9 +37,9 @@ im_array = []
 for y in range(im.height):
     im_array.append([])
     for x in range(im.width):
-        brightness_level = int(reduce(operator.add, im.getpixel((x,y)))/3)
-        symbol_index = int(len(symbols) * (brightness_level / 255))
-        im_array[y].append(symbols[symbol_index-1])
+        brightness_level = active_mode(im.getpixel((x,y)))
+        symbol_index = int(len(symbols) * (brightness_level / 255)) - 1 
+        im_array[y].append(symbols[symbol_index])
 
 for row in range(len(im_array)):
     for col in range(len(im_array[row])):
